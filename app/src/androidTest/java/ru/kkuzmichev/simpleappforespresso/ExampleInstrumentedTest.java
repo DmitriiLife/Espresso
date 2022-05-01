6ddb1e4ad3ebb991ea1b2786dc3b3365ff57c9e9
mac <dmitri-shadri@yandex.ru>
2022-05-01 12:53:42 +0300

    first commit

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -6,9 +6,12 @@ import android.content.Context;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,10 +21,8 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
-    }
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
 }
